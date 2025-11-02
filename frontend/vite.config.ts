f05094654e3c69bd182b5b3b6b73fe77e50106b0
allisonlzhang <allisonlzhang03@gmail.com>
2025-11-02 18:26:04 -0500

    fix base url path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/thirddegree/' : '/', // Only use /thirddegree/ for production builds
+  base: process.env.NODE_ENV === 'production' ? '/3rdDegree/' : '/', // Match GitHub Pages repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
